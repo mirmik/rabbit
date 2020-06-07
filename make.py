768485ce9d688f1fab045643d933e74c0612649d
netricks <netricks@protonmail.com>
2020-06-08 00:17:14 +0300

    'a'

--- make.py
+++ make.py
@@ -4,6 +4,7 @@
 import os
 import shutil
 import licant
+import licant.install
 
 licant.include("rabbit", "rabbit.g.py")
 
@@ -22,14 +23,23 @@ licant.cxx_shared_library("librabbit.so",
 	cc_flags="-fPIC"
 )
 
-@licant.routine(deps=["librabbit.so"])
-def install():
-	os.system("cp {0} {1}".format(target, install_directory_path))
-	
-	shutil.rmtree(install_include_path, True)
-	shutil.copytree("rabbit", install_include_path, 
-		symlinks=False, ignore=shutil.ignore_patterns('*.cpp', '*.c'))
-	
-	print("successfully installed")
+
+licant.install.install_library(
+	tgt="install",
+	uninstall="uninstall",
+	libtgt="librabbit.so",
+	hroot="rabbit",
+	headers="rabbit")
+
+
+#@licant.routine(deps=["librabbit.so"])
+#def install():
+#	os.system("cp {0} {1}".format(target, install_directory_path))
+#	
+#	shutil.rmtree(install_include_path, True)
+#	shutil.copytree("rabbit", install_include_path, 
+#		symlinks=False, ignore=shutil.ignore_patterns('*.cpp', '*.c'))
+#	
+#	print("successfully installed")
 
 licant.ex("librabbit.so")
