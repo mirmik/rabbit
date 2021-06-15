b63b8e6e01ab24aec13404b8206b1b3ac25713b6
mirmik <mirmikns@yandex.ru>
2021-06-15 21:31:20 +0300

    'a'

--- opengl_tests/font/main.cpp
+++ opengl_tests/font/main.cpp
@@ -1,12 +1,6 @@
-#include <rabbit/opengl/opengl_shader_program.h>
-#include <rabbit/opengl/projection.h>
-#include <rabbit/opengl/drawer.h>
-#include <rabbit/opengl/shader_collection.h>
-#include <rabbit/mesh.h>
-#include <rabbit/util.h>
-#include <rabbit/space/pose3.h>
 
-#include <rabbit/space/htrans.h>
+
+#include <iostream>
 
 // GLEW
 #define GLEW_STATIC
@@ -15,80 +9,182 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
-// GLM Mathematics
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+// Other Libs
+#include <SOIL.h>
 
-#include <rabbit/opengl/util.h>
-#include <rabbit/opengl/drawer.h>
-//#include <rabbit/chart/chart.h>
-#include <rabbit/camera.h>
+// Other includes
+#include "Shader.h"
 
-#include <ralgo/linalg/vecops.h>
 #include <nos/print.h>
 
-#include <igris/tuple.h>
-#include <igris/systime.h>
-
-#include <chrono>
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 // Window dimensions
-const GLuint WIDTH = 1600, HEIGHT = 800;
+const GLuint WIDTH = 800, HEIGHT = 600;
 
+// The MAIN function, from here we start the application and run the game loop
 int main()
 {
-	glfwInit();
+    // Init GLFW
+    glfwInit();
+    // Set all the required options for GLFW
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
 
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
-	glfwMakeContextCurrent(window);
+    // Set the required callback functions
+    glfwSetKeyCallback(window, key_callback);
 
-	glewExperimental = GL_TRUE;
-	glewInit();
+    // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+    glewExperimental = GL_TRUE;
+    // Initialize GLEW to setup the OpenGL Function pointers
+    glewInit();
 
-	glViewport(WIDTH*0.1, HEIGHT*0.1, WIDTH*0.8, HEIGHT*0.8);
-
-
-
-
-
-
-	//rabbit::opengl_drawer drawer;
-
-	nos::println("HERE");
-	rabbit::opengl_drawer drawer;
-	drawer.init_opengl_context();
-
-	nos::println("HERE");
-	float aspect = (float)WIDTH / (float)HEIGHT;
-	rabbit::mat4 projection =
-	    rabbit::opengl_perspective(rabbit::deg(100) / aspect, aspect, 0.1, 100);
-
-/*	rabbit::opengl_shader_program sprg(
-	    rabbit::simple_vertex_shader,
-	    rabbit::simple_fragment_shader);
-*/
-	igris::start_local_time();
-	while (!glfwWindowShouldClose(window))
-	{
-		nos::println("HERE");
-		glfwPollEvents();
-		nos::println("HERE");
-		drawer.clean(0.2f, 0.3f, 0.3f, 1.0f);
-		nos::println("HERE1");
-		//sprg.use();
-		drawer.set_vertices_stride(3);
+    // Define the viewport dimensions
+    glViewport(0, 0, WIDTH, HEIGHT);
 
 
-		//glUseProgram(0);
-		glfwSwapBuffers(window);
-	}
+    // Build and compile our shader program
+    Shader ourShader(
+    	"/home/mirmik/project/rabbit/opengl_tests/font/textures.vs", 
+    	"/home/mirmik/project/rabbit/opengl_tests/font/textures.frag");
 
-	glfwTerminate();
-	return 0;
+
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat vertices[] = {
+        // Positions          // Colors           // Texture Coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // Top Right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // Bottom Right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // Bottom Left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // Top Left 
+    };
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3, // First Triangle
+        1, 2, 3  // Second Triangle
+    };
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+    // TexCoord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0); // Unbind VAO
+
+
+    // Load and create a texture 
+    GLuint texture1;
+    GLuint texture2;
+    // ====================
+    // Texture 1
+    // ====================
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1); // All upcoming GL_TEXTURE_2D operations now have effect on our texture object
+
+   // Load, create texture and generate mipmaps
+    int width=100, height=100;
+    //unsigned char* image = SOIL_load_image("container.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+    
+    unsigned char image[width * height];
+
+    for (int i = 0; i < width; ++i)
+    	for(int j = 0; j < height; ++j) 
+    	{
+    		image[i * width + j] = (i + j) % 2 ? 255 : 0;
+    	}
+
+    glTexImage2D(GL_TEXTURE_2D, 
+    	0, 
+    	GL_RED, 
+    	width, 
+    	height, 
+    	0, 
+    	GL_RED, 
+    	GL_UNSIGNED_BYTE, 
+    	image
+    );
+    
+    glGenerateMipmap(GL_TEXTURE_2D);
+    //SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
+    // ===================
+    // Texture 2
+    // ===================
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    // Load, create texture and generate mipmaps
+    unsigned char* image2 = SOIL_load_image("awesomeface.png", &width, &height, 0, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image2);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image2);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+
+    // Game loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        glfwPollEvents();
+
+        // Render
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Activate shader
+        ourShader.Use();     
+
+        // Bind Textures using texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture2"), 1);  
+        
+        // Draw container
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // Swap the screen buffers
+        glfwSwapBuffers(window);
+    }
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
+    return 0;
 }
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
