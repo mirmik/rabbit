152b01e8f0962464b24ef038903e489b52bf9632
mirmik <mirmikns@yandex.ru>
2021-12-29 13:30:07 +0300

    clean

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mirmik/project/radioline/rfmeask/submodules/rabbit
-# Build directory: /home/mirmik/project/radioline/rfmeask/submodules/rabbit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(rabbit_test "/home/mirmik/project/radioline/rfmeask/submodules/rabbit/rabbit_test")
-set_tests_properties(rabbit_test PROPERTIES  _BACKTRACE_TRIPLES "/home/mirmik/project/radioline/rfmeask/submodules/rabbit/CMakeLists.txt;58;add_test;/home/mirmik/project/radioline/rfmeask/submodules/rabbit/CMakeLists.txt;0;")
