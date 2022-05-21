80ee2dd82554b731d445819d0edc18cd6b8d31c2
mirmik <mirmikns@yandex.ru>
2022-05-21 08:57:29 +0300

    'arr'

--- rabbit/chart/qtchart.h
+++ rabbit/chart/qtchart.h
@@ -2,61 +2,31 @@
 #include <QtOpenGL>
 #include <rabbit/chart/chart.h>
 
-namespace rabbit 
+namespace rabbit
 {
 	class qtchart : public QOpenGLWidget, protected QOpenGLFunctions
 	{
 		QOpenGLShaderProgram * shader;
-		//QOpenGLBuffer * vertex_array_buffer;
-		//QOpenGLBuffer * element_array_buffer;
-		//QOpenGLBuffer * index_array_buffer;
-
-		//QOpenGLVertexArrayObject * vertex_array_object;
-
-
 		int posAttribute;
 
-		const char * chart_vertex_shader  = R"""(#version 300 es
-layout (location = 0) in vec2 position;
-
-void main()
-{
-    gl_Position = vec4(position, 0.0f, 1.0f);
-}
-)""";
-
-		const char * chart_fragment_shader = R"""(#version 300 es
-
-uniform lowp vec3 Color;
-out lowp vec4 color;
-
-void main()
-{
-    color = vec4(Color, 1.0f);
-} 
-)""";		
-
-    const char* vertexShaderSource =
-        "#version 330 core\n"
-        "in vec2 posAttr;\n"
-        //"attribute lowp vec3 colAttr;\n"
-        //"varying lowp vec4 col;\n"
-        //"uniform highp mat4 matrix;\n"
-        "void main() {\n"
-        //"   col = colAttr;\n"
-        "   gl_Position = vec4(posAttr, 0, 1) ;\n"
-        "}\n";
+		const char* vertexShaderSource =
+		    "#version 330 core\n"
+		    "in vec2 posAttr;\n"
+		    //"attribute lowp vec3 colAttr;\n"
+		    //"varying lowp vec4 col;\n"
+		    //"uniform highp mat4 matrix;\n"
+		    "void main() {\n"
+		    //"   col = colAttr;\n"
+		    "   gl_Position = vec4(posAttr, 0, 1) ;\n"
+		    "}\n";
 
 
-	const char* fragmentShaderSource = R"(
-    #version 330 core
-
-    out vec4 fragColor;
-
-    void main() {
-        fragColor = vec4(1.0, 0.0, 1.0, 0.0);
-    }
-)";
+		const char* fragmentShaderSource =
+    		"#version 330 core\n"
+		    "out vec4 fragColor;\n"
+		    "void main() {\n"
+        	"fragColor = vec4(1.0, 0.0, 1.0, 0.0);\n"
+    		"}\n";
 
 	public:
 		qtchart(QWidget *parent = 0) 
