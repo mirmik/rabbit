207b07e92998f56679b36db2f4712b67fe025d4d
mirmik <mirmikns@yandex.ru>
2023-01-05 11:32:29 +0300

    'refactor'

--- rabbit/camera.h
+++ rabbit/camera.h
@@ -1,104 +1,109 @@
 #ifndef RABBIT_CAMERA_H
 #define RABBIT_CAMERA_H
 
+#include <rabbit/types.h>
 #include <ralgo/space/pose3.h>
 
 namespace rabbit
 {
-	class camera
-	{
-		vec3f _eye;
-		vec3f _dir;
-		vec3f _up;
+    class camera
+    {
+        vec3f _eye;
+        vec3f _dir;
+        vec3f _up;
 
-	public:
-		camera() : _eye{0, 0, 0}, _dir(1, 0, 0), _up(0, 0, 1)
-		{
-			correct_up();
-		}
+    public:
+        camera() : _eye{0, 0, 0}, _dir(1, 0, 0), _up(0, 0, 1)
+        {
+            correct_up();
+        }
 
-		camera(
-		    const vec3f & eye,
-		    const vec3f & target,
-		    const vec3f & up = {0, 0, 1}
-		)
-			: _eye(eye), _dir(normalize(eye - target)), _up(normalize(up))
-		{
-			correct_up();
-		}
+        camera(const vec3f &eye,
+               const vec3f &target,
+               const vec3f &up = {0, 0, 1})
+            : _eye(eye), _dir(normalize(eye - target)), _up(normalize(up))
+        {
+            correct_up();
+        }
 
-		vec3f right()
-		{
-			return cross(_up, _dir);
-		}
+        vec3f right()
+        {
+            return cross(_up, _dir);
+        }
 
-		void correct_up()
-		{
-			auto _right = right();
-			
-			if (length(_right) < 1e-5)
-			{
-				if (_dir.z > 0.99)
-					_up = {1, 0, 0};
-				else
-					_up = {0, 0, 1};
-				_right = right();
-			}
+        void correct_up()
+        {
+            auto _right = right();
 
-			_up = cross(_dir, _right);
-			_up = normalize(_up);
-		}
+            if (length(_right) < 1e-5)
+            {
+                if (_dir.z > 0.99)
+                    _up = {1, 0, 0};
+                else
+                    _up = {0, 0, 1};
+                _right = right();
+            }
 
-		void set_eye(const vec3f& vec) { _eye = vec; }
-		void set_eye(const vec3& vec) { _eye = vec3f{ (float)vec.x, (float)vec.y, (float)vec.z }; }
-		void set_dir(const vec3f& vec) { _dir = normalize(vec); }
-		void set_target(const vec3f& target)
-		{
-			_dir = normalize(_eye - target);
-			correct_up();
-		}
+            _up = cross(_dir, _right);
+            _up = normalize(_up);
+        }
 
-		void set_camera(vec3f eye, vec3f target, vec3f up=vec3f{0, 0, 1})
-		{
-			_eye = eye;
-			_dir = normalize(eye - target);
-			_up = normalize(up);
-			correct_up();
-		}
+        void set_eye(const vec3f &vec)
+        {
+            _eye = vec;
+        }
 
-		mat4f view_rotation_matrix()
-		{
-			vec3f& D = _dir;
-			vec3f& U = _up;
-			vec3f  R = normalize(cross(_up, _dir));
+        void set_eye(const vec3 &vec)
+        {
+            _eye = vec3f{(float)vec.x, (float)vec.y, (float)vec.z};
+        }
 
-			return
-			{
-				{R.x, U.x, D.x, 0},
-				{R.y, U.y, D.y, 0},
-				{R.z, U.z, D.z, 0},
-				{0, 0, 0, 1}
-			};
-		}
+        void set_dir(const vec3f &vec)
+        {
+            _dir = normalize(vec);
+        }
 
-		mat4f view_translation_matrix()
-		{
-			vec3f& C = _eye;
+        void set_target(const vec3f &target)
+        {
+            _dir = normalize(_eye - target);
+            correct_up();
+        }
 
-			return
-			{
-				{  1.f,  0.f,  0.f, 0.f},
-				{  0.f,  1.f,  0.f, 0.f},
-				{  0.f,  0.f,  1.f, 0.f},
-				{ -C.x, -C.y, -C.z, 1.f}
-			};
-		}
+        void set_camera(vec3f eye, vec3f target, vec3f up = vec3f{0, 0, 1})
+        {
+            _eye = eye;
+            _dir = normalize(eye - target);
+            _up = normalize(up);
+            correct_up();
+        }
 
-		mat4f view_matrix()
-		{
-			return mul(view_rotation_matrix(), view_translation_matrix());
-		}
-	};
+        mat4f view_rotation_matrix()
+        {
+            vec3f &D = _dir;
+            vec3f &U = _up;
+            vec3f R = normalize(cross(_up, _dir));
+
+            return {{R.x, U.x, D.x, 0},
+                    {R.y, U.y, D.y, 0},
+                    {R.z, U.z, D.z, 0},
+                    {0, 0, 0, 1}};
+        }
+
+        mat4f view_translation_matrix()
+        {
+            vec3f &C = _eye;
+
+            return {{1.f, 0.f, 0.f, 0.f},
+                    {0.f, 1.f, 0.f, 0.f},
+                    {0.f, 0.f, 1.f, 0.f},
+                    {-C.x, -C.y, -C.z, 1.f}};
+        }
+
+        mat4f view_matrix()
+        {
+            return mul(view_rotation_matrix(), view_translation_matrix());
+        }
+    };
 }
 
 #endif
--- rabbit/mesh/mesh.h
+++ rabbit/mesh/mesh.h
@@ -1,147 +1,146 @@
 #ifndef RABBIT_MESH_H
 #define RABBIT_MESH_H
 
-#include <rabbit/third/linalg.h>
 #include <rabbit/geom/surface.h>
+#include <rabbit/third/linalg.h>
 #include <rabbit/third/stl_reader.h>
 #include <vector>
 
 namespace rabbit
 {
-	template <class T>
-	class mesh
-	{
-	public:
-		std::vector<linalg::vec<T, 3>> vertices;
-		std::vector<linalg::vec<unsigned int, 3>> triangles;
+    template <class T = float> class mesh
+    {
+    public:
+        std::vector<linalg::vec<T, 3>> vertices;
+        std::vector<linalg::vec<unsigned int, 3>> triangles;
 
-	public:
-		mesh(
-		    const std::vector<linalg::vec<T, 3>>& _vertices,
-    		const std::vector<linalg::vec<unsigned int, 3>>& _triangles
-		) : vertices(_vertices), triangles(_triangles)
-		{}
+    public:
+        mesh(const std::vector<linalg::vec<T, 3>> &_vertices,
+             const std::vector<linalg::vec<unsigned int, 3>> &_triangles)
+            : vertices(_vertices), triangles(_triangles)
+        {
+        }
 
-		mesh() = default;
+        mesh() = default;
 
-		mesh(
-		    std::vector<linalg::vec<T, 3>>&& _vertices,
-    		std::vector<linalg::vec<unsigned int, 3>>&& _triangles
-			) : vertices(std::move(_vertices)), triangles(std::move(_triangles))
-		{}
+        mesh(std::vector<linalg::vec<T, 3>> &&_vertices,
+             std::vector<linalg::vec<unsigned int, 3>> &&_triangles)
+            : vertices(std::move(_vertices)), triangles(std::move(_triangles))
+        {
+        }
 
-		linalg::vec<T,3> center() 
-		{
-			linalg::vec<double, 3> acc = { 0, 0, 0 };
-			int n = vertices.size();
-		
-			for (auto & v : vertices) 
-			{
-				acc += v;
-			}
-		
-			return linalg::vec<T,3> { acc / n };
-		}
-		
-		
-		void correct_center() 
-		{
-			auto c = center();
-		
-			for (auto & v : vertices) 
-			{
-				v -= c;
-			}
-		}
-	};
+        linalg::vec<T, 3> center()
+        {
+            linalg::vec<double, 3> acc = {0, 0, 0};
+            int n = vertices.size();
 
-	template <class T>
-	mesh<T> surface_rubic_mesh(
-	    const surface & surf,
-	    T ustrt, T ufini, int utotal,
-	    T vstrt, T vfini, int vtotal)
-	{
-		int iter;
-	
-		std::vector<linalg::vec<T, 3>> vertices;
-		std::vector<linalg::vec<unsigned int, 3>> triangles;
-	
-		vertices.resize((utotal + 1) * (vtotal + 1));
-		triangles.resize(2 * utotal * vtotal);
-	
-		iter = 0;
-		for (int j = 0; j < vtotal + 1; ++j)
-		{
-			for (int i = 0; i < utotal + 1; ++i)
-			{
-				T ku = ((T)i / (T)utotal);
-				T kv = ((T)j / (T)vtotal);
-	
-				T u = ustrt * ((T)1. - ku) + ufini * ku;
-				T v = vstrt * ((T)1. - kv) + vfini * kv;
-	
-				auto val = surf.value(u, v);
-				vertices[iter++] = { (T)val.x, (T)val.y, (T)val.z };
-			}
-		}
-	
-		iter = 0;
-		for (int j = 0; j < vtotal; ++j)
-		{
-			for (int i = 0; i < utotal; ++i)
-			{
-				unsigned int a = i + (    j) * (utotal + 1);
-				unsigned int b = i + (    j) * (utotal + 1) + 1;
-				unsigned int c = i + (1 + j) * (utotal + 1);
-				unsigned int d = i + (1 + j) * (utotal + 1) + 1;
-	
-				triangles[iter++] = {a, b, c};
-				triangles[iter++] = {d, c, b};
-			}
-		}
-	
-		return mesh<T>(std::move(vertices), std::move(triangles));
-	}
+            for (auto &v : vertices)
+            {
+                acc += v;
+            }
 
+            return linalg::vec<T, 3>{acc / n};
+        }
 
-	template <class T>
-	mesh<T> surface_rubic_mesh(
-	    const surface & surf,
-	    int utotal,
-	    int vtotal)
-	{
-		return surface_rubic_mesh<T>(
-		           surf,
-		           surf.umin(), surf.umax(), utotal,
-		           surf.vmin(), surf.vmax(), vtotal
-		       );
-	}
-	
-	template <class T>
-	mesh<T> mesh_from_file(const char * path)
-	{
-		std::vector<float> coords, normals;
-		std::vector<unsigned int> tris, solids;
-	
-		std::vector<linalg::vec<T, 3>> vertices;
-		std::vector<linalg::vec<unsigned int, 3>> triangles;
-		
-		stl_reader::ReadStlFile ("bulbasaur_dual_body.STL", coords, normals, tris, solids);
-		
-		int vertices_total = coords.size() / 3;
-		int triangles_total = tris.size() / 3;
-	
-		vertices.resize(vertices_total);
-		triangles.resize(triangles_total);
-	
-		for (unsigned int i = 0; i < vertices_total; ++i) 
-			vertices[i] = { coords[i*3], coords[i*3+1], coords[i*3+2] };  
-	
-		for (unsigned int i = 0; i < triangles_total; ++i) 
-			triangles[i] = { tris[i*3], tris[i*3+1], tris[i*3+2] };  
-	
-		return mesh<T>(std::move(vertices), std::move(triangles));
-	}
+        void correct_center()
+        {
+            auto c = center();
+
+            for (auto &v : vertices)
+            {
+                v -= c;
+            }
+        }
+    };
+
+    template <class T = float>
+    mesh<T> surface_rubic_mesh(const surface &surf,
+                               T ustrt,
+                               T ufini,
+                               int utotal,
+                               T vstrt,
+                               T vfini,
+                               int vtotal)
+    {
+        int iter;
+
+        std::vector<linalg::vec<T, 3>> vertices;
+        std::vector<linalg::vec<unsigned int, 3>> triangles;
+
+        vertices.resize((utotal + 1) * (vtotal + 1));
+        triangles.resize(2 * utotal * vtotal);
+
+        iter = 0;
+        for (int j = 0; j < vtotal + 1; ++j)
+        {
+            for (int i = 0; i < utotal + 1; ++i)
+            {
+                T ku = ((T)i / (T)utotal);
+                T kv = ((T)j / (T)vtotal);
+
+                T u = ustrt * ((T)1. - ku) + ufini * ku;
+                T v = vstrt * ((T)1. - kv) + vfini * kv;
+
+                auto val = surf.value(u, v);
+                vertices[iter++] = {(T)val.x, (T)val.y, (T)val.z};
+            }
+        }
+
+        iter = 0;
+        for (int j = 0; j < vtotal; ++j)
+        {
+            for (int i = 0; i < utotal; ++i)
+            {
+                unsigned int a = i + (j) * (utotal + 1);
+                unsigned int b = i + (j) * (utotal + 1) + 1;
+                unsigned int c = i + (1 + j) * (utotal + 1);
+                unsigned int d = i + (1 + j) * (utotal + 1) + 1;
+
+                triangles[iter++] = {a, b, c};
+                triangles[iter++] = {d, c, b};
+            }
+        }
+
+        return mesh<T>(std::move(vertices), std::move(triangles));
+    }
+
+    template <class T = float>
+    mesh<T> surface_rubic_mesh(const surface &surf, int utotal, int vtotal)
+    {
+        return surface_rubic_mesh<T>(surf,
+                                     surf.umin(),
+                                     surf.umax(),
+                                     utotal,
+                                     surf.vmin(),
+                                     surf.vmax(),
+                                     vtotal);
+    }
+
+    template <class T = float> mesh<T> mesh_from_file(const char *path)
+    {
+        std::vector<float> coords, normals;
+        std::vector<unsigned int> tris, solids;
+
+        std::vector<linalg::vec<T, 3>> vertices;
+        std::vector<linalg::vec<unsigned int, 3>> triangles;
+
+        stl_reader::ReadStlFile(
+            "bulbasaur_dual_body.STL", coords, normals, tris, solids);
+
+        int vertices_total = coords.size() / 3;
+        int triangles_total = tris.size() / 3;
+
+        vertices.resize(vertices_total);
+        triangles.resize(triangles_total);
+
+        for (unsigned int i = 0; i < vertices_total; ++i)
+            vertices[i] = {coords[i * 3], coords[i * 3 + 1], coords[i * 3 + 2]};
+
+        for (unsigned int i = 0; i < triangles_total; ++i)
+            triangles[i] = {tris[i * 3], tris[i * 3 + 1], tris[i * 3 + 2]};
+
+        return mesh<T>(std::move(vertices), std::move(triangles));
+    }
 }
 
 #endif
